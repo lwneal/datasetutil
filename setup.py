166237fd9fc3502757cc969db364ad639360ca93
Lawrence Neal <nealla@lwneal.com>
2018-07-30 18:47:24 -0700

    Increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='datasetutil',
-    version='0.1.0',
+    version='0.1.1',
     description='Simple image dataset format, with several useful download scripts',
     author='Larry Neal',
     author_email='nealla@lwneal.com',
